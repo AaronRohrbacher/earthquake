9aa7a31d8727efb4f80fcbcef99040185fa6171f
Aaron Rohrbacher and Keegan Ruebling <aaron@>
2017-10-19 10:57:46 -0700

    seperate business and interface logic

--- js/earthquake-interface.js
+++ js/earthquake-interface.js
@@ -1,62 +1,39 @@
+import { TremorTron } from './../js/earthquake.js';
 
 $(document).ready(function() {
   $('.input').submit(function(event) {
     $('#earthquake').text("")
     event.preventDefault();
+
     let search = $('#search').val();
+    let startDate = $('#startDate').val();
+    let endDate = $('#endDate').val();
+    let sort = $('#sort').val();
+    let javi = new TremorTron(search, startDate, endDate, sort);
+    let searchQuery = javi.callApiSearch(search);
 
-    let promise1 = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${search}&key=AIzaSyBb-6lyykgnZhSEv_FdW6BWi_7BjznhOmw`;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    }
-    request.open("GET", url, true);
-    request.send();
-  });
-
-  promise1.then(function(response) {
-    let body = JSON.parse(response);
-    let lat = body.results[0].geometry.location.lat;
-    let lng = body.results[0].geometry.location.lng;
-
-    let promise2 = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&longitude=${lng}&latitude=${lat}&maxradius=.5&starttime=2017-01-01`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise2.then(function(response) {
+    searchQuery.then(function(response) {
       let body = JSON.parse(response);
-      if (body.features.length > 0) {
-      body.features.forEach(function(earthquake) {
-        $('#earthquake').append(`<tr><td>${earthquake.properties.place}</td><td>${new Date(earthquake.properties.time)}</td><td>${earthquake.properties.mag}</td><td>${earthquake.properties.tsunami}</td></tr> `)
+      let lat = body.results[0].geometry.location.lat;
+      let lng = body.results[0].geometry.location.lng;
+      let quakeQuery = javi.callApiQuake(lat, lng, startDate, endDate, sort);
+      $('#earthquake').append("EARTHQUAKETRON LOADING<img src = 'img/loading.gif'>")
+
+      quakeQuery.then(function(response) {
+        $('#earthquake').text('')
+        let body = JSON.parse(response);
+        if (body.features.length > 0) {
+        body.features.forEach(function(earthquake) {
+          $('#earthquake').append(`<tr><td>${earthquake.properties.place}</td><td>${new Date(earthquake.properties.time)}</td><td>${earthquake.properties.mag}</td><td>${earthquake.properties.tsunami}</td><td>${earthquake.properties.alert}</td><td>${earthquake.properties.felt}</td></tr> `)
+        });
+      } else {
+        $('#earthquake').text("Tain't no dirt movin'");
+      }
+        }, function(error) {
+        $('#showErrors').text(`Go to hell: ${error.message}`);
       });
-    } else {
-      $('#earthquake').text("Tain't no dirt movin'");
-
-    }
       }, function(error) {
-      $('#showErrors').text(`Go to hell: ${error.message}`);
+      $('#showErrors').text(`Hell won't take you: ${error.message}`);
     });
-
-  }, function(error) {
-    $('#showErrors').text(`Hell won't take you: ${error.message}`);
-  });
-
-  // $('#earthquake').text(body.features[0].properties.place);
-
   });
 });
